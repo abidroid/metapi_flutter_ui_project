1f77be26978133763f8514bc18b3ea530d412a95
Abid <abidroid1@gmail.com>
2026-03-26 17:56:41 +0500

    container part 2

--- lib/screens/container_demo_screen.dart
+++ lib/screens/container_demo_screen.dart
@@ -6,29 +6,63 @@ class ContainerDemoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
       appBar: AppBar(
         backgroundColor: Colors.blue,
         title: Text('Container Demo'),
       ),
 
-      body: Container(
-        width: 300,
-        height: 300,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
+      body: Center(
+        child: Container(
+          // inside the container
+          padding: EdgeInsets.only(left: 10, top: 20),
 
-          border: Border.all(color: Colors.blue, width: 5),
+          // outside the container
+          margin: EdgeInsets.only(left: 10, top: 10),
+          width: 300,
+          height: 300,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
 
-          gradient: LinearGradient(
-            colors: [Colors.pink, Colors.purple, Colors.brown, Colors.cyan],
+            border: Border.all(color: Colors.blue, width: 5),
 
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight
+            // gradient: LinearGradient(
+            //   colors: [Colors.pink, Colors.purple, Colors.brown, Colors.cyan],
+            //
+            //   begin: Alignment.topLeft,
+            //   end: Alignment.bottomRight
+            // ),
+            gradient: RadialGradient(
+              center: Alignment.center,
+              colors: [Colors.orange, Colors.pink, Colors.blue],
+            ),
+
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                offset: Offset(-5, -5),
+                spreadRadius: 5,
+                blurRadius: 4
+              ),
+
+              BoxShadow(
+                  color: Colors.purple,
+                  offset: Offset(10, 5),
+                  spreadRadius: 5,
+                  blurRadius: 4
+              ),
+            ]
+          ),
+
+          child: Text(
+            'Sun',
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.white,
+              backgroundColor: Colors.green,
+
+            ),
           ),
         ),
-
-        child: Text('Sun', style: TextStyle(fontSize: 30)),
       ),
     );
   }
