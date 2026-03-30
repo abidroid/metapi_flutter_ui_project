eefba5b526821447d93154b334ab9ba4b0db64fb
Abid <abidroid1@gmail.com>
2026-03-30 16:45:33 +0500

    stack completed

--- lib/screens/stack_demo_screen.dart
+++ lib/screens/stack_demo_screen.dart
@@ -6,26 +6,42 @@ class StackDemoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.teal,
-        title: Text('Stack Demo'),
-      ),
+      appBar: AppBar(backgroundColor: Colors.teal, title: Text('Stack Demo')),
 
       body: Stack(
         clipBehavior: Clip.none,
         children: [
-          Container(width: 300, height: 300, color: Colors.red,),
-          Container(width: 200, height: 200, color: Colors.green,),
-          Container(width: 100, height: 100, color: Colors.blue,),
+          Container(width: 300, height: 300, color: Colors.red),
+          Container(width: 200, height: 200, color: Colors.green),
+          Container(width: 100, height: 100, color: Colors.blue),
 
           Positioned(
-              bottom: -25,
-              left: 125,
+            bottom: -50,
+            left: 100,
 
-              child: Container(width: 50, height: 50, color: Colors.black,)),
+            child: Container(
+                width: 100,
+                height: 100,
+                color: Colors.black,
+               child: Column(
+                 spacing: 10,
+                 mainAxisAlignment: .center,
+                 children: [
+                   Row(
+                     mainAxisAlignment: .spaceAround,
+                     children: [
+                       Container(width: 30, height: 20, color: Colors.white,),
+                       Container(width: 30, height: 20, color: Colors.white,),
+                     ],
+                   ),
+                   Container(width: 10, height: 30, color: Colors.white,),
+                   Container(width: 80, height: 20, color: Colors.white,)
+                 ],
+               ),
+            ),
+          ),
         ],
       ),
     );
   }
 }
-
